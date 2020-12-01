1df9e9894db798feba020efe326d66e0d6959ebe
evergreen2001 <dicksonevergreenido@gmail.com>
2020-12-01 03:27:36 +0100

    added input fields

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-'use stirct '
+"use stirct ";
 
-let url = "https://reqres.in/api/users?"
+let url = "https://reqres.in/api/users?";
 
+let textArea = document.getElementById("textarea");
+let datePicker = document.getElementById("date-picker");
 
+let submitBtn = document.getElementById("contact-submit");
 
+let select = document.getElementById("contact");
 
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  alert(datePicker.value + textArea.value + select.value);
+});
 
-
-  fetch(url)
-  .then(res =>{
-
-    return res.json() 
-
+fetch(url)
+  .then((res) => {
+    return res.json();
   })
 
-  .then(function(data) {
-    let authors = data.data; // Get the results
-   
-    console.log(data.data)
+  .then(function (data) {
+    let contact = data.data; // Get the results
 
-    for(x in authors ){
-        var sel = document.createElement("option");
-        sel.innerHTML = authors[x].first_name;
-        sel.value  = authors[x].first_name;
-        document.getElementById("contact").appendChild(sel);
-      }
-      
-  })
+    console.log(data.data);
+
+    for (x in contact) {
+      var sel = document.createElement("option");
+      sel.innerHTML = contact[x].first_name + " " + contact[x].last_name;
+      sel.value = contact[x].first_name + " " + contact[x].last_name;
+      document.getElementById("contact").appendChild(sel);
+    }
+  });
