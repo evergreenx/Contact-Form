18e4bd83f5542a31c2bf727be2aa80dbc43ffe50
evergreen2001 <dicksonevergreenido@gmail.com>
2020-12-02 15:42:12 +0100

    pushing to github

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use stirct ";
-
-let url = "https://reqres.in/api/users?";
+let url = "https://reqres.in/api/users?page=1";
 
 let textArea = document.getElementById("textarea");
 let datePicker = document.getElementById("date-picker");
@@ -11,35 +10,57 @@ let select = document.getElementById("contact");
 let myForm = document.getElementById("myForm");
 let modal = document.querySelector(".overlay");
 let content = document.querySelector(".content");
+let closeModal = document.querySelector(".close");
 
-console.log(myForm);
-
-// myForm.addEventListener("submit", validateForm);
+// submit eventlistener
 
 myForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  // alert(datePicker.value + textArea.value + select.value);
 
   modal.style.display = "block";
-  content.innerText = datePicker.value + textArea.value + select.value;
+
+  if (textArea.value == "") {
+    textArea.value = "No comment";
+  }
+
+  let temp = `
+  <p> Contact: ${select.value} </p> 
+  <p> Date: ${datePicker.value} </p> 
+   <p> Comment: ${textArea.value} <p>
+   `;
+  content.innerHTML = temp;
 
   myForm.reset();
 });
+// submit eventlistener end
 
-fetch(url)
-  .then((res) => {
-    return res.json();
-  })
+// close Modal
+closeModal.addEventListener("click", (e) => {
+  modal.style.display = "none";
+});
 
-  .then(function (data) {
-    let contact = data.data; // Get the results
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
 
-    console.log(data.data);
+// fetch contact from api
+const populateSelect = () => {
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
 
-    for (x in contact) {
-      var sel = document.createElement("option");
-      sel.innerHTML = contact[x].first_name + " " + contact[x].last_name;
-      sel.value = contact[x].first_name + " " + contact[x].last_name;
-      document.getElementById("contact").appendChild(sel);
-    }
-  });
+    .then(function (data) {
+      let contact = data.data; // Get the results
+
+      for (x in contact) {
+        var sel = document.createElement("option");
+        sel.innerHTML = contact[x].first_name + " " + contact[x].last_name;
+        sel.value = contact[x].first_name + " " + contact[x].last_name;
+        document.getElementById("contact").appendChild(sel);
+      }
+    });
+};
+populateSelect();
